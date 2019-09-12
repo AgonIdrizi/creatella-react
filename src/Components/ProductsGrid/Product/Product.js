68e6d6e7784380e2dd129634b581a9a8937c7e0c
AgonIdrizi <agon.88@hotmail.com>
2019-09-12 15:52:45 +0200

    products component style change

--- src/Components/ProductsGrid/Product/Product.js
+++ src/Components/ProductsGrid/Product/Product.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Card } from 'antd';
 import Face from './Face/Face';
+import {
+  formatDate,
+  formatCentsToDollars,
+} from '../../../utilFunctions/formaterUtils';
 import 'antd/es/card/style/index.css';
-import './Product';
+import './Product.css';
 
-const cardStyle = { width: 150, height: 150 };
+const cardStyle = { width: '100%' };
 
-const Product = props => {
+const Product = ({ face, size, price, date }) => {
   return (
     <div className="Product">
-      <Card name="test" hoverable={true} bordered={true} style={cardStyle}>
-        <Face face={props.face} size={props.size} />
-        <p>Size: {props.size} </p>
-        <p>Price: {props.price}</p>
-        <p>Date: {props.date}</p>
+      <Card
+        name="test"
+        hoverable={true}
+        bordered={true}
+        style={{ width: '100%' }}
+      >
+        <Face face={face} size={size} />
+        <p>
+          <span className="span-product">Size:</span> {size}{' '}
+        </p>
+        <p>
+          <span className="span-product">Price:</span>{' '}
+          {formatCentsToDollars(price)}
+        </p>
+        <p>
+          <span className="span-product">Date added:</span> {formatDate(date)}
+        </p>
       </Card>
     </div>
   );
