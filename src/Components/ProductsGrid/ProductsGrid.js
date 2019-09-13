804c5a5131e4ff747ee33eb36b63012f85a53f5c
AgonIdrizi <agon.88@hotmail.com>
2019-09-13 19:35:48 +0200

    make ProductsGrid scroll infinitely

--- src/Components/ProductsGrid/ProductsGrid.js
+++ src/Components/ProductsGrid/ProductsGrid.js
@@ -1,36 +1,140 @@
 import React, { useState, useEffect } from 'react';
 import Product from './Product/Product';
+import { Spin } from 'antd';
+import 'antd/es/spin/style/index.css';
 import axios from 'axios';
 import './ProductsGrid.css';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'start':
+      return { ...state, loading: false };
+    case 'load':
+      return {
+        ...state,
+        data: action.payload,
+        isLoading: false,
+        nextPage: state.nextPage + 1,
+      };
+    case 'preFetch':
+      return {
+        ...state,
+        preFetchedData: action.payload,
+        isPreFetching: false,
+      };
+    case 'setIsFetching': {
+      return {
+        ...state,
+        isPreFetching: true,
+      };
+    }
+    case 'bottom':
+      //nextPage keeps track of what should be our page to preFetch data 'api/products?_page=1'
+      //we update nextPage, only when user scrolls at the bottom and we are not in process of preFetching data
+      //e.g. when we dispatch the bottom action in handleScroll function,
+      //we dont want to update nextPage if we are in the procces of preFetching data
+      const nextPage = state.isPreFetching
+        ? state.nextPage
+        : state.nextPage + 1;
+      return {
+        ...state,
+        data: [...state.data, ...state.preFetchedData],
+        preFetchedData: [],
+        nextPage: nextPage,
+      };
+    default:
+      throw new Error();
+  }
+};
+
 const ProductsGrid = props => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [state, dispatch] = React.useReducer(reducer, {
+    data: [],
+    isLoading: true,
+    preFetchedData: [],
+    isPreFetching: false,
+    nextPage: 2,
+  });
+
+  const { data, preFetchedData, isLoading, isPreFetching, nextPage } = state;
+
+  //this effect is used to add a scroll event-listener, so we can detect when the page is at bottom,
+  //we also fetch the first batch of data here
+  //when component unmounts, the cleaner function of this effect will remove our scroll-event that we added early on
+  //this effect will run only once, that's why we have empty array dependencies [] as second argument passed
   useEffect(() => {
+    console.log('first effect');
+    window.addEventListener('scroll', handleScroll);
     axios
       .get('http://localhost:3000/api/products?_page=1&_limit=15')
       .then(response => {
-        setData(response.data);
-        console.log(response.data);
+        dispatch({ type: 'load', payload: response.data });
       })
       .catch(error => console.log(error));
 
-    return () => console.log('returning');
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+
+  //second effect will run after the first effect
+  //and then will re-run every time when state of nextPage changes
+  useEffect(() => {
+    console.log('second effect');
+    dispatch({ type: 'setIsFetching' });
+    axios
+      .get(`http://localhost:3000/api/products?_page=${nextPage}&_limit=15`)
+      .then(response => {
+        dispatch({ type: 'preFetch', payload: response.data });
+      })
+      .catch(error => console.log(error));
+  }, [nextPage]);
+
+  const handleScroll = () => {
+    const scrollTop =
+      window.document.documentElement.scrollTop ||
+      window.document.body.scrollTop;
+    const scrollHeight =
+      window.document.documentElement.scrollHeight ||
+      window.document.body.scrollHeight;
+
+    const scrolledToBottom = scrollTop + window.innerHeight >= scrollHeight;
+
+    if (scrolledToBottom) {
+      dispatch({ type: 'bottom' });
+    }
+  };
+
   return (
     <section className="ProductsGrid">
-      {loading && <div>loading</div>}
+      {isLoading && <Spin size="large" />}
       {data &&
         data.length > 0 &&
-        data.map(product => (
-          <Product
-            key={product.id}
-            size={product.size}
-            price={product.price}
-            face={product.face}
-            date={product.date}
-          />
-        ))}
+        data.map((product, index) => {
+          return (index + 1) % 20 === 0 ? (
+            [
+              <Product
+                key={product.id}
+                size={product.size}
+                price={product.price}
+                face={product.face}
+                date={product.date}
+              />,
+              <div key={`spacer-${product.id}`}>text</div>,
+            ]
+          ) : (
+            <Product
+              key={product.id}
+              size={product.size}
+              price={product.price}
+              face={product.face}
+              date={product.date}
+            />
+          );
+        })}
+      {isPreFetching  && !isLoading && <Spin />}
+      {!isPreFetching &&
+        preFetchedData.length === 0 &&
+        !isLoading &&
+        '~ end of catalogue ~'}
     </section>
   );
 };
